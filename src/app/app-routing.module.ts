f2082afef32056c3a6cd50f984db7e83126ced52
Mateus <orlandi.mateus@gmail.com>
2024-12-04 20:30:43 -0300

    6th commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import {PaginaInicialComponent} from "./componentes/pagina-inicial/pagina-inicial.component";
-import {VisualizarTarefasComponent} from "./componentes/visualizar-tarefas/visualizar-tarefas.component";
 import {CarrinhoComponent} from "./componentes/carrinho/carrinho.component";
 import {ListaDesejosComponent} from "./componentes/lista-desejos/lista-desejos.component";
 
